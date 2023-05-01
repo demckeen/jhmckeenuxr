f97e3d8f62a3c70c4d6acfd6f28282bf857a01e6
zircadia <76924125+zircadia@users.noreply.github.com>
2023-05-01 17:34:34 -0500

    updated links

--- pages/writing/index.js
+++ pages/writing/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Splash from '../../components/common/splash'
+import Link from 'next/link'
 
 export default function Writing() {
   return (
@@ -13,25 +14,25 @@ export default function Writing() {
       <Splash pageTitle="UX Writing" mainSaying="" textBody="" actionButton="" imgUrl="/images/typewriter.jpg" imgAlt="a typewriter sitting on dark rustic wood plank surface"/>
       <div className="writing container">
         <div className="card blog" id="1">
-          <a href="/writing/generative-vs-tactical">
+          <Link href="/writing/generative-vs-tactical">
           <span className="type">blog</span>
           <p className="title">Generative vs. Tactical Research</p>
           <p className="tagline"></p>
-          </a>
+          </Link>
         </div>
         <div className="card article" id="2">
-          <a href="/writing/title">
+          <Link href="/writing/title">
           <span className="type">article</span>
           <p className="title"></p>
           <p className="tagline"></p>
-          </a>
+          </Link>
         </div>
         <div className="card blog" id="3">
-          <a href="/writing/title">
+          <Link href="/writing/title">
           <span className="type">blog</span>
           <p className="title"></p>
           <p className="tagline"></p>
-          </a>
+          </Link>
         </div>
       </div>
     </div>
