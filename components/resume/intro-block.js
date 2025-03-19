fa98ef3456f48852634f105eae0394054e6452e8
zircadia <76924125+zircadia@users.noreply.github.com>
2025-03-18 20:02:54 -0500

    updated resume file

--- components/resume/intro-block.js
+++ components/resume/intro-block.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Intro = () => {
   return (
     <section className="section intro">
-        <a target="_blank" href="/JHMResume2023.pdf"><i className="mdi mdi-file-pdf-box"></i>Download PDF Version</a>
+        <a target="_blank" href="/JHMResume2025.pdf"><i className="mdi mdi-file-pdf-box"></i>Download PDF Version</a>
       <div className="intro-summary">
         <h2>Summary</h2>
         <p>I am a behavioral researcher by training, and a UX researcher by profession. My academic focus was spatial cognition with an emphasis on wayfinding/navigation. I spent my entire academic career documenting the cognitive strategies of people getting lost in complex, real-world environments. Now I spend my entire professional career documenting the cognitive strategies of people getting lost in complex, digital environments. They parallel one another more than I had ever imagined!</p>
